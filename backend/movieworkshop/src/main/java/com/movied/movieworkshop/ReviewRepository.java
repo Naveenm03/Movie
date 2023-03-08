98faa43c8f39ecbd7efac5a79437f034a4182a34
Naveenm03 <107742777+Naveenm03@users.noreply.github.com>
2023-03-08 11:01:29 +0530

    Update ReviewRepository.java

--- backend/movieworkshop/src/main/java/com/movied/movieworkshop/ReviewRepository.java
+++ backend/movieworkshop/src/main/java/com/movied/movieworkshop/ReviewRepository.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
+@Transactional
+	@Modifying
+	@Query("delete from Review e where e.reviews=:reviews")
+	void deleteReviewByDesig(@Param("reviews")String reviews);
+
 
 
 
